0e54a9809c3f892a0a3bb4da27530d7ad4fff606
SumitRana <rana.sumit93@gmail.com>
2020-12-14 15:52:02 +0530

    Initial Commit

---
+++ Code/wsgi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys
+import logging
+logging.basicConfig(stream=sys.stderr)
+
+appPath = "/var/www/Code/"
+sys.path.insert(0,appPath)
+
+from FlaskApp import app as application
+application.secret_key = 'Add your secret key'
